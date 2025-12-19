679093a7098c3b9eed451b4aa1a5c66ae3a9356d
Sahilvashisth001 <Sahilvashisth001@gmail.com>
2025-12-19 15:34:43 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,44 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import {ENV} from './lib/env.js';
-import { connectDB } from './lib/db.js';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
 
 dotenv.config();
 
-const app=express();
+const app = express();
 
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
-app.get("/health",(req,res)=>{
-    res.status(200).json({msg:"success from api"});
-})
+//middleware
+app.use(express.json());
+//credentials true meaning => server allow a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-//make our app ready for deployment 
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "success from api" });
+});
+
+//make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server running on port ${ENV.PORT}`);
     });
-}
-
-const startServer=async()=>{
-    try{
-        await connectDB();
-        app.listen(ENV.PORT,()=>{
-            console.log(`Server running on port ${ENV.PORT}`);
-        });
-    }catch(err){
-        console.log("Failed to start server:",err);
-    }
-}
+  } catch (err) {
+    console.log("Failed to start server:", err);
+  }
+};
 startServer();
