6367272b16cd805a5d6e5d8951e6ca7f1a7007e4
Sahilvashisth001 <Sahilvashisth001@gmail.com>
2026-01-07 03:51:44 +0530

    imporve server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,65 +1,49 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
-import { inngest, functions } from "./lib/inngest.js";
-import dotenv from "dotenv";
-import { ENV } from "./lib/env.js";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoute.js";
 
-dotenv.config();
-
 const app = express();
 
-// ===== FIX __dirname FOR ES MODULE =====
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
-// ===== MIDDLEWARE =====
+// middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: ENV.CLIENT_URL,
-    credentials: true,
-  })
-);
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use(clerkMiddleware());
-
-// ===== API ROUTES =====
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-// ===== FRONTEND SERVING (PRODUCTION) =====
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use(express.static(frontendPath));
-
-  // SPA fallback (VERY IMPORTANT)
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-// ===== START SERVER =====
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log(`Server running on port ${ENV.PORT}`);
-    });
-  } catch (err) {
-    console.log("Failed to start server:", err);
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
   }
 };
 
