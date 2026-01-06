84de5041c78c27e1de16325b4d2de271801a4344
Sahilvashisth001 <Sahilvashisth001@gmail.com>
2026-01-07 00:20:47 +0530

    session page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from "react";
 import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  UserButton,
-  useUser,
+  useUser
 } from "@clerk/clerk-react";
 
-import { Navigate,Routes,Route } from "react-router";
-import HomePage from "./pages/HomePage.jsx";
-import DashboardPage from "./pages/DashboardPage.jsx";
-import ProblemsPage from "./pages/ProblemsPage.jsx";
 import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 import ProblemPage from "./pages/ProblemPage.jsx";
+import ProblemsPage from "./pages/ProblemsPage.jsx";
+import SessionPage from "./pages/SessionPage.jsx";
 
 
 function App() {
@@ -32,7 +27,8 @@ function App() {
       <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
       <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
       <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
-       <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+      <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+       <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
     </Routes>
     <Toaster  toastOptions={{duration:3000}}/>
     </>
