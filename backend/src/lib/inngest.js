5e823e31d7b6ac7eddff951356a9473d5174fda7
Sahilvashisth001 <Sahilvashisth001@gmail.com>
2026-05-10 00:16:17 +0530

    debugg inngest(new version)

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,13 +1,65 @@
+// import { Inngest } from "inngest";
+// import { connectDB } from "./db.js";
+// import User from "../models/User.js";
+// import { upsertStreamUser, deleteStreamUser } from "./stream.js";
+
+// export const inngest = new Inngest({ id: "CodeArena" });
+// //function to sync user from clerk to our db
+// const syncUser = inngest.createFunction(
+//   { id: "sync-user" },
+//   { event: "clerk/user.created" },
+//   async ({ event }) => {
+//     await connectDB();
+
+//     const { id, email_addresses, first_name, last_name, image_url } =
+//       event.data;
+
+//     const newUser = {
+//       clerkId: id,
+//       email: email_addresses[0]?.email_address,
+//       name: `${first_name || ""} ${last_name || ""}`,
+//       profileImage: image_url,
+//     };
+//     //save user to db
+//     await User.create(newUser);
+
+//     //also create user in stream
+//     await upsertStreamUser({
+//       id: newUser.clerkId.toString(),
+//       name: newUser.name,
+//       image: newUser.profileImage,
+//     });
+//   }
+// );
+// //function to delete user from our db when deleted from clerk
+// const deleteUserFromDB = inngest.createFunction(
+//   { id: "delete-user" },
+//   { event: "clerk/user.deleted" },
+//   async ({ event }) => {
+//     await connectDB();
+
+//     const { id } = event.data;
+//     await User.deleteOne({ clerkId: id });
+
+//     //also delete user from stream
+//     await deleteStreamUser(id.toString());
+//   }
+// );
+
+// export const functions = [syncUser, deleteUserFromDB];
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
 import { upsertStreamUser, deleteStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "CodeArena" });
-//function to sync user from clerk to our db
+
+// Function to sync user from Clerk to our DB
 const syncUser = inngest.createFunction(
-  { id: "sync-user" },
-  { event: "clerk/user.created" },
+  {
+    id: "sync-user",
+    triggers: [{ event: "clerk/user.created" }],
+  },
   async ({ event }) => {
     await connectDB();
 
@@ -17,13 +69,14 @@ const syncUser = inngest.createFunction(
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
       profileImage: image_url,
     };
-    //save user to db
+
+    // Save user to DB
     await User.create(newUser);
 
-    //also create user in stream
+    // Also create user in Stream
     await upsertStreamUser({
       id: newUser.clerkId.toString(),
       name: newUser.name,
@@ -31,17 +84,21 @@ const syncUser = inngest.createFunction(
     });
   }
 );
-//function to delete user from our db when deleted from clerk
+
+// Function to delete user from DB when deleted from Clerk
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user" },
-  { event: "clerk/user.deleted" },
+  {
+    id: "delete-user",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
   async ({ event }) => {
     await connectDB();
 
     const { id } = event.data;
+
     await User.deleteOne({ clerkId: id });
 
-    //also delete user from stream
+    // Also delete user from Stream
     await deleteStreamUser(id.toString());
   }
 );
